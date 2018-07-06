fee82390c3be672a12dfe3bcc214d023ff71ea14
Vinay Krisha <vinaykrishna1989@gmail.com>
2018-07-06 11:06:49 +0530

    generated components for poll-add/poll-list/poll-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PollsComponent } from './polls/polls.component';
+import { PollListComponent } from './polls/poll-list/poll-list.component';
+import { PollAddComponent } from './polls/poll-add/poll-add.component';
+import { PollDetailComponent } from './polls/poll-detail/poll-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PollsComponent,
+    PollListComponent,
+    PollAddComponent,
+    PollDetailComponent
   ],
   imports: [
     BrowserModule
